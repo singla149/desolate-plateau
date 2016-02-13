2b11ccd4e5ff86dd8f1420c690c09a321979ae6e
Vidur Katyal <vidurkatyal95@gmail.com>
2016-02-13 19:03:21 +0530

    Added save to file and menu functions

--- main.py
+++ main.py
@@ -15,9 +15,33 @@ def init():
 	with open("files/enrollments.json", "r") as f:
 		enrollments = json.load(f)
 
+def save():
+	with open("files/students.json", "w") as f:
+		json.dump(students, f)
+	with open("files/courses.json", "w") as f:
+		json.dump(courses, f)
+	with open("files/enrollments.json", "w") as f:
+		json.dump(enrollments, f)
+
 def menu():
-	# Add menu here
-	pass
+	print "Welcome!"
+	while True:
+		print "1.Add a Student"
+		print "2.Delete a Student"
+		print "3. Look Up Student Record"
+		print "4.Exit/Quit"
+		ch = raw_input("What would you like to do? ")
+		if ch == "1":
+			pass
+		elif ch == "2":
+			pass
+		elif ch == "3":
+			pass
+		elif ch == "4":
+			print "\nBye!\n"
+			break
+		else:
+			print "\nNot Valid Choice! Try again!\n"
 
 if __name__ == "__main__":
 	init()

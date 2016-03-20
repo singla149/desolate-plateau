9cd138b91148f2ba6099f3aa4482d0761bee1624
Tushar Sinha <sinha.tushar94@gmail.com>
2016-03-20 13:53:22 +0530

    added delete archives with course

--- views/delete_course.py
+++ views/delete_course.py
@@ -15,6 +15,9 @@ def delete_course():
         with open("files/enrollments.json", "r") as f:
             enrollments = json.load(f)
         c_id = request.form[key]
+        archived_enrolls = {}
+        with open("files/archived_enrolls.json", "r") as f:
+            archived_enrolls = json.load(f)
 
         if c_id not in courses:
             return "\n\nERROR! No such course exists!\n\n"
@@ -36,11 +39,20 @@ def delete_course():
             return redirect(url_for('index'))
         for removal in all_removals:
             del enrollments[removal[0]+"##@@##"+c_id]
+        all_removals = []
+        for enroll in archived_enrolls:
+            if c_id == enroll.split("##@@##")[1]:
+                removal = [enroll.split("##@@##")[0], archived_enrolls[enroll]["doe"]]
+                all_removals.append(removal)
+        for removal in all_removals:
+            del archived_enrolls[removal[0]+"##@@##"+c_id]
         del courses[c_id]
         with open("files/courses.json", "w") as f:
             json.dump(courses, f)
         with open("files/enrollments.json", "w") as f:
             json.dump(enrollments, f)
+        with open("files/archived_enrolls.json", "w") as f:
+            json.dump(archived_enrolls, f)
         return render_template("delete.html", obj = obj, keyval = c_id,
                                 flag = 3, name = name,
                                 )

1a7bd7f83a0edb15015dc3dde0b3583ce8fd6375
Vidur Katyal <vidurkatyal95@gmail.com>
2016-03-21 00:28:39 +0530

    Added list by branches

--- views/__init__.py
+++ views/__init__.py
@@ -12,6 +12,7 @@ from add_branch import *
 
 from list_enrollments import *
 from archive_enrollments import *
+from list_by_branches import *
 
 from modify_student import *
 from modify_course import *
---
+++ views/list_by_branches.py
@@ -0,0 +1,76 @@
+from flask import render_template, session, flash, request
+from flask_wtf import Form
+from wtforms import StringField, SubmitField, TextAreaField, IntegerField, SelectField
+from wtforms.fields.html5 import DateField
+from wtforms.validators import DataRequired, Length, Regexp, NumberRange
+
+import json
+import datetime
+
+from main import app
+
+class ListByBranchForm(Form):
+    b_id = SelectField('Branch:', validators=[DataRequired()])
+    submit =  SubmitField('List!')
+
+@app.route('/list-branches/course/', methods =["GET", "POST"])
+def list_courses_by_branch():
+    obj = "Courses"
+    form = ListByBranchForm()
+    branches = {}
+    with open("files/branches.json", "r") as f:
+        branches = json.load(f)
+    form.b_id.choices = [(b_id, branches[b_id]["name"]) for b_id in sorted(branches)]
+
+    if form.validate_on_submit():
+        courses = {}
+        with open("files/courses.json") as f:
+            courses = json.load(f)
+        branch_courses = []
+        for key in courses:
+            if form.b_id.data == courses[key]["b_id"]:
+                curr = []
+                curr.append(key)
+                curr.append(courses[key]["name"])
+                curr.append(courses[key]["cred"])
+                curr.append(courses[key]["c_type"])
+                branch_courses.append(curr)
+        cols = ["Roll No.", "Name", "Date of Enrollment", "Course Type"]
+        return render_template("list_by_branch.html", form = form, obj = obj, cols = cols,
+                rows = branch_courses)
+    elif request.method == "POST":
+        flash("There seem to be some errors in the form", "danger")
+    return render_template("list_by_branch.html", form = form, obj = obj)
+
+
+@app.route('/list-branches/student/', methods =["GET", "POST"])
+def list_students_by_branch():
+    obj = "Students"
+    form = ListByBranchForm()
+    branches = {}
+    with open("files/branches.json", "r") as f:
+        branches = json.load(f)
+    form.b_id.choices = [(b_id, branches[b_id]["name"]) for b_id in sorted(branches)]
+
+    if form.validate_on_submit():
+        students = {}
+        with open("files/students.json") as f:
+            students = json.load(f)
+        branch_students = []
+        for key in students:
+            if form.b_id.data == students[key]["b_id"]:
+                curr = []
+                curr.append(key)
+                curr.append(students[key]["name"])
+                curr.append(students[key]["dob"])
+                curr.append(students[key]["doa"])
+                curr.append(students[key]["ph_no"])
+                curr.append(students[key]["sex"])
+                curr.append(students[key]["c_type"])
+                branch_students.append(curr)
+        cols = ["Roll No.", "Name", "Date of Birth", "Date of Admission", "Phone No.", "Sex", "Course Type"]
+        return render_template("list_by_branch.html", form = form, obj = obj, cols = cols,
+                rows = branch_students)
+    elif request.method == "POST":
+        flash("There seem to be some errors in the form", "danger")
+    return render_template("list_by_branch.html", form = form, obj = obj)
